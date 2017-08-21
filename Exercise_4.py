7a638634f911a4b1598a5f427b2b768f3108da20
saurabhgg <saurabhggautam@gmail.com>
2017-08-21 14:25:06 +0530

    Exercise_4:Numbers exercise

---
+++ Exercise_4.py
@@ -0,0 +1,12 @@
+#Variables Exercise
+cars=4.0
+tyres=4
+engine_cc=1048.55
+car_height='150 Cms'
+user_name='Simon'
+avg_cost=10588.98
+print user_name,"has ",int(cars)," cars in his garrage"
+#More information about the cars
+print user_name,"has ",int(cars)," super cars in his garrage having a super engine of  ",engine_cc , " cc "
+print user_name," has spent close to ",(avg_cost*cars)/tyres,"on each tyre"
+print user_name+"'s car is ",car_height,"tall"
