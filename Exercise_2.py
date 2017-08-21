19a2813122b5765478561745aef6c5b62f2d072f
saurabhgg <saurabhggautam@gmail.com>
2017-08-21 14:09:54 +0530

    Exercise_2 Addition

---
+++ Exercise_2.py
@@ -0,0 +1,9 @@
+#A sample comment program.By usin "#" we can comment anything or disable a piece of code.
+
+print "A comment program" #This text would be ignored and will not be printed
+
+'''
+This text would be ignored and will not be printedThis text would be ignored and will not be printedThis text would be ignored and will not be printed
+This text would be ignored and will not be printedThis text would be ignored and will not be printedThis text would be ignored and will not be printed'''
+
+print' We can also comment the group of lines by using " ''' " "
