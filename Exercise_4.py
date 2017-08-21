6545d49fda509b9cb704f9c8e59b8cfe865eec28
saurabhgg <saurabhggautam@gmail.com>
2017-08-21 14:28:37 +0530

    Exercise_4:Numbers exercise

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,10 +1,12 @@
 #Variables Exercise
-cars=4.0
-tyres=4
-engine_cc=1048.55
-car_height='150 Cms'
-user_name='Simon'
-avg_cost=10588.98
+#always add space around operators so that its easier to read and its a good practice too.
+
+cars = 4.0
+tyres = 4
+engine_cc = 1048.55
+car_height = '150 Cms'
+user_name = 'Simon'
+avg_cost = 10588.98
 print user_name,"has ",int(cars)," cars in his garrage"
 #More information about the cars
 print user_name,"has ",int(cars)," super cars in his garrage having a super engine of  ",engine_cc , " cc "
